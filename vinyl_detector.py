65d3c2e6736ec4dcb776b6b5c30cbfdbbdd571ad
Guy Zigdon <guyz@hlil-423-lap.qb.hailotech>
2024-09-10 03:46:55 +0300

    Main loop blueprint

--- vinyl_detector.py
+++ vinyl_detector.py
@@ -1,6 +1,15 @@
+from time import sleep
+from gemini_detector.gemini_detector import detect_vinyl
 
 def main() -> None:
-    pass
+    #setup streaming
+    while True:
+        # save image from stream in a file called "image.jpg"
+        # result = detect_vinyl("image.jpg")
+        # send result to frontend
+        sleep(10)
+        pass
+
 
 if __name__ == '__main__':
     main()
